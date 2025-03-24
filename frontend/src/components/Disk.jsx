f6640449dce702f3287d86f1201c60d3ac1b92a0
Jianing Leng <145624450+Synpath@users.noreply.github.com>
2025-03-23 23:31:34 -0400

    Beginning Plate UI implementation
    help this is so difficult and i am so bad at css

--- frontend/src/components/Disk.jsx
+++ frontend/src/components/Disk.jsx
@@ -1,66 +1,20 @@
-import React, { useState, useRef } from "react";
+import React, { useState, useRef, useEffect } from 'react';
+import RecipeCard from './RecipePreview';
+import './css/Disk.css';
 
-const Disk = () => {
-    const [isDragging, setIsDragging] = useState(false);
-    const [angle, setAngle] = useState(0);
-    const startCoords = useRef({x: 0, y: 0});
+const RotatingCircle = () => {
 
-const handleMouseDown = (e) => {
-    setIsDragging(true);
-    startCoords.current = { x: e.clientX, y: e.clientY};
-    document.body.style.userSelect = 'none';
+  return (
+    <div className="plate" >
     
-    document.addEventListener('mousemove', handleMouseMove);
-    document.addEventListener('mouseup', handleMouseUp);
-};
-
-const handleMouseMove = (e) => {
-    if (!isDragging)  {
-        return;
-    }
-
-    const diffX = e.clientX - startCoords.current.x;
-    const diffY = e.clientY - startCoords.current.y;
-
-    const newAngle = Math.atan2(diffY, diffX) * (180 / Math.PI);
-    setAngle(newAngle);
-};
-
-const handleMouseUp = () => {
-    setIsDragging(false);
-    document.removeEventListener('mousemove', handleMouseMove);
-    document.removeEventListener('mouseup', handleMouseUp);
-    document.body.style.userSelect = 'auto';
-};
-
-return (
-    <div
-      className="disk-container"
-      style={{
-        width: '300px',
-        height: '300px',
-        borderRadius: '50%',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: 'red',
-        position: 'relative',
-      }}
-    >
-      <div
-        className="disk"
-        style={{
-          width: '500px',
-          height: '200px',
-          backgroundColor: '#3498db',
-          borderRadius: '50%',
-          cursor: 'pointer',
-          transform: `rotate(${angle}deg)`,
-        }}
-        onMouseDown={handleMouseDown}
-      ></div>
+    <div className="middle">
+    {/*<RecipeCard className="card1" />*/} 
+    
+    </div>
     </div>
   );
 };
 
-export default Disk;
+export default RotatingCircle;
+
+
