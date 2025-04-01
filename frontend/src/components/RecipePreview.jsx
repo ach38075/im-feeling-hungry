4e1f95381e7399805d323cf334e7e37c72463a34
Jianing Leng <145624450+Synpath@users.noreply.github.com>
2025-03-31 23:16:15 -0400

    Added Hover styling to the cards
    yee naw :(

--- frontend/src/components/RecipePreview.jsx
+++ frontend/src/components/RecipePreview.jsx
@@ -6,10 +6,16 @@ const handleShowRecipe = () => {
     alert("Show Full Recipe function not yet available");
 };
 
+const handleClick = () => {
+        alert("Pop-Up Recipe Card function not yet available");
+};
+
 const RecipePreview = ({title, className}) => {
     return (
         
         <div className={`card ${className}`}>
+            <div className="clickable" onClick={handleClick}>
+
             {/* preview card of the recipe
             clickable, title, image, author, cook time, show recipe button */} 
             <img src={ImgSrc} width="200" height="200"/>
@@ -22,7 +28,14 @@ const RecipePreview = ({title, className}) => {
                 <h5>Dietary Restrictions?: Default Value</h5>     
             </div>
 
-            <button onClick={handleShowRecipe}>Show Full Recipe</button>
+            </div>
+
+            <button onClick={(e) => {
+                e.stopPropagation();
+                handleShowRecipe();
+            }}>
+                Show Full Recipe
+            </button>
         </div>
     );
 };
