392ea40aa148dad99d2325639992c6b01181a7a8
Jianing Leng <145624450+Synpath@users.noreply.github.com>
2025-04-02 11:55:09 -0400

    Added Card Shifting functionality
    chat we're so back, silksong is REAL

--- frontend/src/components/Disk.jsx
+++ frontend/src/components/Disk.jsx
@@ -8,23 +8,59 @@ import './css/Disk.css';
 // - Add popup functionality : when you click on a card, it brings up the full recipe card on page in the center of the screen
 //   very big, has its own scroll wheel if recipe is long, contents will eventually be integrated with the api calls
 
+const initialCards = [
+  {id: 1, title:"Egg Noodles", cardName:"card"},
+  {id: 2, title:"Hamburger", cardName:"card"},
+  {id: 3, title:"Dumplings with Pork", cardName:"card"},
+  {id: 4, title:"Broiled Salmon", cardName:"card"},
+  {id: 5, title:"Sashimi with Wasabi", cardName:"card"},
+  {id: 6, title:"Okonomiyaki", cardName:"card"},
+];
 
 const Disk = () => {
+
+  const [cards, setCards] = useState(initialCards);
+
+  const rightShift = () => {
+
+    setCards((prevCards) => 
+      prevCards.map((card) => ({
+        ...card, // Copy card properties
+        id: card.id + 1 > 6 ? 1 : card.id + 1,
+      }))
+    );
+  }; 
+  
+  const leftShift = () => {
+
+    setCards((prevCards) => 
+      prevCards.map((card) => ({
+        ...card, // Copy card properties
+        id: card.id - 1 < 1 ? 6 : card.id - 1,
+      }))
+    );
+  };   
 
   return (
 
     <div className="plate">
     <div className="middle">
      
-    <RecipeCard className="card1"/>
-    <RecipeCard className="card2" />
-    <RecipeCard className="card3" />
-    <RecipeCard className="card4" />
-    <RecipeCard className="card5" />
-    <RecipeCard className="card6" />
-      
+    {cards.map((card) => {
+
+      console.log(card.cardName);
+      return(
+        <RecipeCard className={`${card.cardName + card.id}`} title={`${card.title}`} key={`${card.id}`}/>
+      );
+    })} 
+    
+    
+    <button className="left" onClick={leftShift}>Scroll Cards Left</button>
+    <button className="right" onClick={rightShift}>Scroll Cards Right</button>
     </div>
+    
     </div>
+    
   );
   
 };
